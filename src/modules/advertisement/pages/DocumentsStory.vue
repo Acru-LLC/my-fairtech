d4570e02d10e09cc8c5d10afddb321cc5720164d
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-10-18 15:26:41 +0500

    add layout for manu items

--- src/modules/advertisement/pages/DocumentsStory.vue
+++ src/modules/advertisement/pages/DocumentsStory.vue
@@ -1,11 +1,17 @@
 <script>
+import Layout from "@/router/layouts/main";
 export default {
+  components: {
+    Layout
+  },
 name: "DocumentsStory"
 }
 </script>
 
 <template>
+  <Layout>
   <h1>Hello world</h1>
+  </Layout>
 </template>
 
 <style scoped>
