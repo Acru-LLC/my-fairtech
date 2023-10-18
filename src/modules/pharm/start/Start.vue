f86aa370d3e24699b605f79de6ba350f1f5e38f3
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-10-18 15:44:07 +0500

    moved old version

--- src/modules/pharm/start/Start.vue
+++ src/modules/pharm/start/Start.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="row">
-        <div class="col-12">
-            <div class="col-md-12 text-center">
-                <div class="h4 mb-4 d-inline-block">{{ $t('pharm.menu_title') }}</div>
-            </div>
-            <div class="card" style="min-height: 70vh">
-                <!-- end card-body -->
+  <div class="row">
+    <div class="col-12">
+      <div class="col-md-12 text-center">
+        <div class="h4 mb-4 d-inline-block">{{ $t('pharm.menu_title') }}</div>
+      </div>
+      <div class="card" style="min-height: 70vh">
+        <!-- end card-body -->
 
-                <h4 class="p-5"> Ishlab chiqilmoqda . . .</h4>
-            </div>
-            <!-- end card -->
-        </div>
-        <!-- end col -->
+        <h4 class="p-5"> Ishlab chiqilmoqda . . .</h4>
+      </div>
+      <!-- end card -->
     </div>
+    <!-- end col -->
+  </div>
   <!-- end row -->
 </template>
 <script setup>
