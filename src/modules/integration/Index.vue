f52fd9eb62097da10725676608aaee52e68e3be8
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2023-11-15 09:28:25 +0500

    integration index added

---
+++ src/modules/integration/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Layout>
+    <navbar style="margin-bottom:110px !important;"/>
+      <router-view class="px-4"/>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Navbar from "/src/components/nav-bar.vue";
+import Layout from "/src/router/layouts/main"
+
+export default {
+  name: "Index",
+  components: {
+    Navbar,
+    Layout
+  },
+  data: () => ({  }),
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
