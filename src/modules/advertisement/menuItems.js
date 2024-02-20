dea1de641d4687ae93bcf47967b18021cb762092
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2024-02-20 17:06:44 +0500

    removed unnecessary files and changed some features

--- src/modules/advertisement/menuItems.js
+++
@@ -1,8 +0,0 @@
-import i18n from "@/i18n";
-export default [
-  {
-    id: "advertisementParentMenuId",
-    label: i18n.t("product_dashboard_info.menu_items.documents"),
-    link: {name: "Document"},
-  }
-];
