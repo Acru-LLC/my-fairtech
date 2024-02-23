6080c1fcf7ec01a72fe777642886cf81301e1ba9
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2024-02-23 10:02:08 +0500

    rais part changed

--- src/components/pricing.vue
+++ src/components/pricing.vue
@@ -29,38 +29,46 @@ export default {
   <section class="section pt-5" id="pricing">
     <div class="p-5">
       <b-row class="my-5">
-        <b-col style="border: 2px solid #246256; border-radius: 5px" class="p-3 text-color">
-          <b-row>
-            <b-col cols="4">
+        <b-col style="border-radius: 5px; box-shadow: rgb(201, 219, 215) 2px 4px 8px 0px;"  class="p-3 text-color">
+          <b-row class="h-100">
+            <b-col cols="4" class="d-flex align-items-center">
               <div class="d-flex justify-content-center">
-                <img width="112" height="112" src="@/assets/image/vakant.png" alt="">
+                <img width="112" height="112" style="border-radius: 50%; border: 3px solid #246256" src="@/assets/image/vakant.png" alt="">
               </div>
-              <div class="d-flex justify-content-center">
+<!--              <div class="d-flex justify-content-center">-->
                 <!--              <b-button class="mt-3 btn btn-bg-white"-->
                 <!--                        style="box-shadow: 0 1px 6px 0 #246256!important; color: #246256!important;">-->
                 <!--                Қабулга ёзилиш-->
                 <!--              </b-button>-->
-              </div>
+<!--              </div>-->
             </b-col>
 
             <b-col cols="8">
               <b-row>
-                <h4 class="font-weight-bold">Вакант</h4>
-                  <img style="top: 0; right: 0;" class="position-absolute cursor-pointer" @click="openModal" src="@/assets/image/danger.png" alt="">
+                <h4 class="font-weight-bold text-color">Вакант</h4>
+<!--                  <img style="top: 0; right: 0;" class="position-absolute cursor-pointer" @click="openModal" src="@/assets/image/danger.png" alt="">-->
+
                 <b-modal v-model="showModal" hide-footer title="Вакант">
                   <p>Вакант</p>
                 </b-modal>
               </b-row>
-              <div class="d-flex justify-content-between">
-                <img width="18" height="18" src="@/assets/image/clock-two-thirty-svgrepo-com.svg" alt="">
+              <b-row>
+                <span class="font-weight-bold">Қўмита раиси</span>
+              </b-row>
+              <b-row class="d-flex align-items-center">
+                <i class="fa fa-clock font-size-17"></i>
                 <span class="ml-2">Фуқароларни қабул қилиш кунлари</span>
-              </div>
+              </b-row>
               <b-row class="my-2">
                 <span>Ҳар куни 9:00 дан 11:00 гача</span>
               </b-row>
               <b-row>
                 <img style="width: 17px; margin-right: 10px" src="@/assets/image/phone.svg" alt="">
-                <h4>(71) 123-45-67</h4>
+                <h4 class="text-color">(71) 123-45-67</h4>
+              </b-row>
+              <b-row class="d-flex align-items-center">
+                <i class="fa fa-envelope font-size-17"></i>
+                <h4 class="mb-0 ml-2 text-color">info@antimon.gov.uz</h4>
               </b-row>
               <!--            <b-row>-->
               <!--              <b-button class="mt-3 btn btn-bg-white"-->
@@ -96,25 +104,25 @@ export default {
 <!--            </b-col>-->
 <!--          </b-row>-->
         </b-col>
-        <b-col style="border: 2px solid #246256; border-radius: 5px" class="p-3 mx-3 text-color">
-          <b-row>
-            <b-col cols="4">
+        <b-col style="border-radius: 5px; box-shadow: rgb(201, 219, 215) 2px 4px 8px 0px;"  class="p-3 mx-3 text-color">
+          <b-row class="h-100">
+            <b-col cols="4" class="d-flex align-items-center">
               <div class="d-flex justify-content-center">
-                <img width="112" height="112" src="@/assets/image/qayumov.png" alt="">
+                <img width="112" height="112" style="border-radius: 50%; border: 3px solid #246256" src="@/assets/image/qayumov.png" alt="">
               </div>
-              <div class="d-flex justify-content-center">
+<!--              <div class="d-flex justify-content-center">-->
                 <!--              <b-button class="mt-3 btn btn-bg-white"-->
                 <!--                        style="box-shadow: 0 1px 6px 0 #246256!important; color: #246256!important;">-->
                 <!--                Қабулга ёзилиш-->
                 <!--              </b-button>-->
-              </div>
+<!--              </div>-->
             </b-col>
 
             <b-col cols="8">
               <b-row>
-                <h4 class="font-weight-bold">Қаюмов Асадулло Ўролович</h4>
-                <img style="top: 0; right: 0;" class="position-absolute cursor-pointer" @click="openModal1" src="@/assets/image/danger.png" alt="">
-                <span class="font-weight-bold mx-auto">Раиснинг биринчи ўринбосари</span>
+                <h4 class="font-weight-bold text-color">Қаюмов Асадулло Ўролович</h4>
+<!--                <img style="top: 0; right: 0;" class="position-absolute cursor-pointer" @click="openModal1" src="@/assets/image/danger.png" alt="">-->
+                <span class="font-weight-bold">Раиснинг биринчи ўринбосари</span>
                 <b-modal v-model="showModal1" hide-footer size="lg" title="Қаюмов Асадулло Ўролович">
                   <p>
                     Қаюмов Асадулла Уролович 1984 йил Сурхондарё вилоятида туғилган.
@@ -141,16 +149,20 @@ export default {
                   </p>
                 </b-modal>
               </b-row>
-              <div class="d-flex justify-content-between">
-                <img width="18" height="18" src="@/assets/image/clock-two-thirty-svgrepo-com.svg" alt="">
+              <b-row class="d-flex align-items-center">
+                <i class="fa fa-clock font-size-17"></i>
                 <span class="ml-2">Фуқароларни қабул қилиш кунлари</span>
-              </div>
+              </b-row>
               <b-row class="my-2">
                 <span>Душанба-Жума, 09-00 дан 11-00 гача</span>
               </b-row>
               <b-row>
                 <img style="width: 17px; margin-right: 10px" src="@/assets/image/phone.svg" alt="">
-                <h4>(71) 207-47-00</h4>
+                <h4 class="text-color">(71) 207-47-00</h4>
+              </b-row>
+              <b-row class="d-flex align-items-center">
+                <i class="fa fa-envelope font-size-17"></i>
+                <h4 class="mb-0 ml-2 text-color">info@antimon.gov.uz</h4>
               </b-row>
               <!--            <b-row>-->
               <!--              <b-button class="mt-3 btn btn-bg-white"-->
@@ -165,11 +177,11 @@ export default {
             </b-col>
           </b-row>
         </b-col>
-        <b-col style="border: 2px solid #246256; border-radius: 5px" class="p-3 text-color">
-          <b-row>
-            <b-col cols="4">
+        <b-col style="border-radius: 5px; box-shadow: rgb(201, 219, 215) 2px 4px 8px 0px;" class="p-3 text-color">
+          <b-row class="h-100">
+            <b-col cols="4" class="d-flex align-items-center">
               <div class="d-flex justify-content-center">
-                <img src="@/assets/image/boss-avatar.svg" alt="">
+                <img src="@/assets/image/boss-avatar.svg" style="border-radius: 50%; border: 3px solid #246256" alt="">
               </div>
               <div class="d-flex justify-content-center">
                 <!--              <b-button class="mt-3 btn btn-bg-white"-->
@@ -181,9 +193,9 @@ export default {
 
             <b-col cols="8">
               <b-row>
-                <h4 class="font-weight-bold">Карабаев Фаррух Эсаналиевич</h4>
-                <img style="top: 0; right: 0;" class="position-absolute cursor-pointer" @click="openModal2" src="@/assets/image/danger.png" alt="">
-                <span class="font-weight-bold mx-auto">Раис ўринбосари</span>
+                <h4 class="font-weight-bold text-color">Карабаев Фаррух Эсаналиевич</h4>
+<!--                <img style="top: 0; right: 0;" class="position-absolute cursor-pointer" @click="openModal2" src="@/assets/image/danger.png" alt="">-->
+                <span class="font-weight-bold">Раис ўринбосари</span>
                 <b-modal v-model="showModal2" hide-footer size="lg" title="Карабаев Фаррух Эсаналиевич">
                   <p>
                     Карабаев Фаррух Эсаналиевич 1981 йилда Тошкент вилоятининг Ангрен шаҳрида туғилган.
@@ -200,16 +212,20 @@ export default {
                   </p>
                 </b-modal>
               </b-row>
-              <div class="d-flex justify-content-between">
-                <img width="18" height="18" src="@/assets/image/clock-two-thirty-svgrepo-com.svg" alt="">
+              <b-row class="d-flex align-items-center">
+                <i class="fa fa-clock font-size-17"></i>
                 <span class="ml-2">Фуқароларни қабул қилиш кунлари</span>
-              </div>
+              </b-row>
               <b-row class="my-2">
                 <span>Душанба-Жума, 09-00 дан 11-00 гача</span>
               </b-row>
               <b-row>
                 <img style="width: 17px; margin-right: 10px" src="@/assets/image/phone.svg" alt="">
-                <h4>(71) 207-47-00</h4>
+                <h4 class="text-color">(71) 207-47-00</h4>
+              </b-row>
+              <b-row class="d-flex align-items-center">
+                <i class="fa fa-envelope font-size-17"></i>
+                <h4 class="mb-0 ml-2 text-color">info@antimon.gov.uz</h4>
               </b-row>
               <!--            <b-row>-->
               <!--              <b-button class="mt-3 btn btn-bg-white"-->
