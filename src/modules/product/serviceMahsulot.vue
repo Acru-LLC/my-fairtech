37b9394318d44b2492508deb84d72a3744681c72
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2024-03-12 11:54:37 +0500

    added TF TIN search translate

--- src/modules/product/serviceMahsulot.vue
+++ src/modules/product/serviceMahsulot.vue
@@ -115,7 +115,7 @@
         <b-row>
           <b-col cols="1" style="text-align: center">
                         <span style="opacity: 0.7; color: #2b675b">{{
-                            $t('submodules.integration.bojxona_info.number_commodity')
+                            $t('bojxona_info.number_commodity')
                           }}</span> <br>
             <span style="color: #2b675b; font-weight: lighter; font-size: 14px">  {{
                 item.number_commodity
@@ -123,7 +123,7 @@
           </b-col>
           <b-col cols="3" style="text-align: center">
                         <span style="opacity: 0.7; color: #2b675b">{{
-                            $t('submodules.integration.bojxona_info.receiver_name')
+                            $t('bojxona_info.receiver_name')
                           }}</span> <br>
             <span style="color: #2b675b; font-weight: lighter; font-size: 14px">  {{
                 item.receiver_name
@@ -131,7 +131,7 @@
           </b-col>
           <b-col cols="5" style="text-align: center">
                         <span style="opacity: 0.7; color: #2b675b">{{
-                            $t('submodules.integration.bojxona_info.product_name')
+                            $t('bojxona_info.product_name')
                           }}</span> <br>
             <span style="color: #2b675b; font-weight: lighter; font-size: 14px">  {{
                 item.product_name
@@ -139,7 +139,7 @@
           </b-col>
           <b-col cols="2" style="text-align: center">
                           <span style="opacity: 0.7; color: #2b675b">{{
-                              $t('submodules.integration.bojxona_info.made_in_country')
+                              $t('bojxona_info.made_in_country')
                             }}</span> <br>
             <span style="color: #2b675b; font-weight: lighter; font-size: 14px; text-align: center !important;">  {{
                 item.made_in_country
@@ -148,7 +148,7 @@
 
           <b-col cols="1" style="text-align: center">
                         <span style="opacity: 0.7; color: #2b675b">{{
-                            $t('submodules.integration.bojxona_info.tn_code')
+                            $t('bojxona_info.tn_code')
                           }}</span> <br>
             <span style="color: #2b675b; font-weight: lighter; font-size: 14px">  {{
                 item.tn_code
